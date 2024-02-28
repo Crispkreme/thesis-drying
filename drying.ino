32467b37bce1ce4dda3270b846754109afa89f43
Marvin M. Ramos <115031416+Crispkreme@users.noreply.github.com>
2024-02-28 14:52:07 +0800

    Update drying.ino

--- drying.ino
+++ drying.ino
@@ -1,34 +1,25 @@
-#include <DHT.h>;
+int LED = 13; // Use the onboard Uno LED
+int isObstaclePin = 7; // This is our input pin
+int isObstacle = HIGH; // HIGH MEANS NO OBSTACLE
 
-//Constants
-#define DHTPIN 2     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
-
-
-//Variables
-int chk;
-float hum;  //Stores humidity value
-float temp; //Stores temperature value
-
-void setup()
-{
-  Serial.begin(9600);
-	dht.begin();
+void setup() {
+    pinMode(LED, OUTPUT);
+    pinMode(isObstaclePin, INPUT);
+    Serial.begin(9600);
 }
 
-void loop()
-{
-  //Read data and store it to variables hum and temp
-  hum = dht.readHumidity();
-  temp= dht.readTemperature();
-  //Print temp and humidity values to serial monitor
-  Serial.print("Humidity: ");
-  Serial.print(hum);
-  Serial.print(" %, Temp: ");
-  Serial.print(temp);
-  Serial.println(" Celsius");
-  delay(2000); //Delay 2 sec.
-}
+void loop() {
 
-   
+    isObstacle = digitalRead(isObstaclePin);
+
+    if (isObstacle == LOW) {
+        Serial.println(“OBSTACLE!!, OBSTACLE!!”);
+        digitalWrite(LED, HIGH);
+    } else {
+        Serial.println(“clear”);
+        digitalWrite(LED, LOW);
+    }
+
+    delay(200);
+
+}
