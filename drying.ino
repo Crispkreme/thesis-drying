887d3fb8fdf44d83bde516654475f6a8a2b917ad
Marvin M. Ramos <115031416+Crispkreme@users.noreply.github.com>
2024-02-28 15:27:41 +0800

    Update drying.ino

--- drying.ino
+++ drying.ino
@@ -1,25 +1,39 @@
-int LED = 13; // Use the onboard Uno LED
-int isObstaclePin = 7; // This is our input pin
-int isObstacle = HIGH; // HIGH MEANS NO OBSTACLE
+#include <Stepper.h>
+
+// Define pin numbers
+const int proximitySensorPin = 7;
+const int LED_PIN = LED_BUILTIN; 
+
+// Define stepper motor parameters
+const int stepsPerRevolution = 200;
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 
 void setup() {
-    pinMode(LED, OUTPUT);
-    pinMode(isObstaclePin, INPUT);
+    pinMode(LED_PIN, OUTPUT); 
+    myStepper.setSpeed(60);
+    pinMode(proximitySensorPin, INPUT);
     Serial.begin(9600);
 }
 
 void loop() {
+    // Read the state of the proximity sensor
+    int proximitySensorState = digitalRead(proximitySensorPin);
 
-    isObstacle = digitalRead(isObstaclePin);
-
-    if (isObstacle == LOW) {
-        Serial.println(“OBSTACLE!!, OBSTACLE!!”);
-        digitalWrite(LED, HIGH);
+    if (proximitySensorState == LOW) {
+        // Obstacle detected
+        digitalWrite(LED_PIN, HIGH); 
+        Serial.println("OBSTACLE DETECTED!!");
+        Serial.println("Rotating Clockwise");
+        myStepper.step(stepsPerRevolution); 
+        delay(500);
     } else {
-        Serial.println(“clear”);
-        digitalWrite(LED, LOW);
+        // No obstacle detected
+        digitalWrite(LED_PIN, LOW);
+        Serial.println("No Obstacle Detected");
+        Serial.println("Rotating Counterclockwise");
+        myStepper.step(-stepsPerRevolution);
+        delay(500);
     }
 
-    delay(200);
-
+    delay(200); // Delay for stability
 }
